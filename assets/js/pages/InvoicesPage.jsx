85e860ed5a8ed6e4cc0becd09f3bc8cb85b1ab2f
Karim <saber.karim1991@gmail.com>
2020-04-26 21:39:37 +0000

    Installation de react et gestion des customers et invoices

--- assets/js/pages/InvoicesPage.jsx
+++ assets/js/pages/InvoicesPage.jsx
@@ -3,8 +3,7 @@ import Pagination from "../components/Pagination";
 import axios from 'axios';
 import moment from 'moment';
 import InvoicesAPI from "../services/invoicesAPI";
-//hello bro
-//thank u
+
 const STATUS_CLASSES = {
     PAID: "success",
     SENT: "primary",
